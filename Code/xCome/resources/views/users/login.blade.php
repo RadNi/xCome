90adc0b77b0b049e01096199dad516602493e392
System Administrator <root@RadNis-MacBook-Pro.local>
2018-04-29 18:20:31 +0430

    source code directory refreshed

--- Code/xCome/resources/views/users/login.blade.php
+++ Code/xCome/resources/views/users/login.blade.php
@@ -23,12 +23,12 @@
 <form action="" method="post" novalidate>
     {{--        <input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
     {{ csrf_field() }}
-    <input type="email" name="email" placeholder="email"><br>
-    <input type="password" name="password" placeholder="password"><br>
-    <input type="name" name="name" placeholder="name"><br>
-    <input type="age" name="age" placeholder="age"><br>
-    <input type="address" name="address" placeholder="address"><br>
-    <input type="submit" value="register">
+    <input id="email" type="email" name="email" placeholder="email"><br>
+    <input id="password" type="password" name="password" placeholder="password"><br>
+    <input id="" type="name" name="name" placeholder="name"><br>
+    <input id="" type="age" name="age" placeholder="age"><br>
+    <input id="" type="address" name="address" placeholder="address"><br>
+    <input id="" type="submit" value="register">
 
 </form>
 @stop
--- Code/xCome/resources/views/users/register.blade.php
+++ Code/xCome/resources/views/users/register.blade.php
@@ -4,22 +4,23 @@
     <form action={{ url("/register") }} method="post" novalidate>
         {{--        <input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
         {{ csrf_field() }}
-        <input type="email" name="email" placeholder="Email"><br>
-        <input type="password" name="password" placeholder="Password"><br>
-        <input type="password" name="retryPass" placeholder="Repeat Password"><br>
-        <input type="name" name="name" placeholder="Name"><br>
-        <input type="name" name="familyName" placeholder="Family"><br>
-        <input type="name" name="username" placeholder="Username"><br>
-        <input type="age" name="age" placeholder="age"><br>
-        <input type="address" name="address" placeholder="address"><br>
-        <input type="text" name="captcha" placeholder="captcha"><br>
-        <input type="text" name="PersonID" placeholder="Person ID"><br>
-        <input type="submit" value="register">
+        <input id="email" type="email" name="email" placeholder="Email"><br>
+        <input id="password" type="password" name="password" placeholder="Password"><br>
+        <input id="repass" type="password" name="retryPass" placeholder="Repeat Password"><br>
+        <input id="name" type="name" name="name" placeholder="Name"><br>
+        <input id="family" type="name" name="familyName" placeholder="Family"><br>
+        <input id="username" type="name" name="username" placeholder="Username"><br>
+        {{--<input id="" type="age" name="age" placeholder="age"><br>--}}
+        <input id="address" type="address" name="address" placeholder="address"><br>
+        <input id="captcha" type="text" name="captcha" placeholder="captcha"><br>
+        <input id="person_id" type="text" name="PersonID" placeholder="Person ID"><br>
+        <input id="cellphone" type="text" name="CellPhone" placeholder="Phone Number"><br>
+        <input id="submit" type="submit" value="register">
 
     </form>
 
     @if($check)
-        <p id="notValidate">input was wrong, please try again!</p>
+        <p id="inValid">input was wrong, please try again!</p>
     @endif
 @stop
 
