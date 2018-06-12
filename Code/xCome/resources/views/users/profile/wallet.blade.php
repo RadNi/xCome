70f09d6228c222c3038996b6abe108bb0542975d
RadNi <amirhossein_kh@ymail.com>
2018-06-13 03:20:09 +0430

    wallet page edited consider to implement redirection when clicking on every wallet to trading related currency

--- Code/xCome/resources/views/users/profile/wallet.blade.php
+++ Code/xCome/resources/views/users/profile/wallet.blade.php
@@ -2,39 +2,29 @@
 
 @section('workplace')
     <div id="wp-wallets">
-    <table id="wallets-table">
-        <td>
-            <div class="wallets">
-                <p class="wallet-name">Sapphire</p>
-                <p class="rel-price"></p>
-                <p class="diagram"></p>
-                <p class="redirection"></p>
-            </div>
-        </td>
-        <td>
-            <div class="wallets">
-                <p class="wallet-name">Riall</p>
-                <p class="rel-price"></p>
-                <p class="diagram"></p>
-                <p class="redirection"></p>
-            </div>
-        </td>
-        <td>
-            <div class="wallets">
-                <p class="wallet-name">Dollar</p>
-                <p class="rel-price"></p>
-                <p class="diagram"></p>
-                <p class="redirection"></p>
-            </div>
-        </td>
-        <td>
-            <div class="wallets">
-                <p class="wallet-name">Euro</p>
-                <p class="rel-price"></p>
-                <p class="diagram"></p>
-                <p class="redirection"></p>
-            </div>
-        </td>
+    <table id="wallets-table" cellpadding="10px">
+        <tbody>
+            <tr class="wallet" onclick="location.href = '/'">
+                <td class="wallet-name">Sapphire</td>
+                <td class="rel-price">Real time price</td>
+                <td class="diagram">Price diagram</td>
+            </tr>
+            <tr class="wallet" onclick="location.href = '#'">
+                <td class="wallet-name">Riall</td>
+                <td class="rel-price">Real time price</td>
+                <td class="diagram">Price diagram</td>
+            </tr>
+            <tr class="wallet" onclick="location.href = '#'">
+                <td class="wallet-name">Dollar</td>
+                <td class="rel-price">Real time price</td>
+                <td class="diagram">Price diagram</td>
+            </tr>
+            <tr class="wallet" onclick="location.href = '#'">
+                <td class="wallet-name">Euro</td>
+                <td class="rel-price">Real time price</td>
+                <td class="diagram">Price diagram</td>
+            </tr>
+        </tbody>
     </table>
 </div>
 @stop
