a87699f49dfd9ef9323f0117561ef1580b66de42
RadNi <amirhossein_kh@ymail.com>
2018-06-12 22:03:16 +0430

    apply payment page added

---
+++ Code/xCome/resources/views/users/profile/apply-pay.blade.php
@@ -0,0 +1,15 @@
+@extends('users.profile.profile')
+
+@section('workplace')
+    <div id="wp-apply-pay">
+        <form method="post" action="{{ route("profile.apply-pay") }}">
+            {{ csrf_field() }}
+            <input id="payee-id" name="payee-id" placeholder="Payee credit card" type="number">
+            <input id="prirce" name="price" placeholder="price" type="number">$
+            <p id="fee">fee for this transaction</p>
+            <input id="submit" name="submit" type="submit">
+        </form>
+    </div>
+@stop
+
+
