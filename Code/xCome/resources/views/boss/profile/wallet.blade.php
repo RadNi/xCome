58e0d8cd5e91e6dd6fd0c836536596e8a2df729b
RadNi <amirhossein_kh@ymail.com>
2018-06-17 17:00:59 +0430

    wallet address popup added to user and boss page

--- Code/xCome/resources/views/boss/profile/wallet.blade.php
+++ Code/xCome/resources/views/boss/profile/wallet.blade.php
@@ -2,32 +2,33 @@
 
 @section('workplace-div')
     <div id="wp-wallets">
-    <table id="wallets-table" cellpadding="10px">
-        <tbody>
-            <tr class="wallet" onclick="location.href = '#'">
+        <table id="wallets-table" cellpadding="10px" border="1px">
+            <tbody>
+            <tr class="wallet" onclick="address.hidden = false;address.innerHTML = 'wallet address: 381920831'">
                 <td class="wallet-name">Sapphire</td>
                 <td class="rel-price">Real time price</td>
                 <td class="diagram">Price diagram</td>
             </tr>
 
-            <tr class="wallet" onclick="location.href = '#'">
+            <tr class="wallet" onclick="address.hidden = false;address.innerHTML = 'wallet address: 38912'">
                 <td class="wallet-name">Riall</td>
                 <td class="rel-price">Real time price</td>
                 <td class="diagram">Price diagram</td>
             </tr>
-            <tr class="wallet" onclick="location.href = '#'">
+            <tr class="wallet" onclick="address.hidden = false;address.innerHTML = 'wallet address: 481924'">
                 <td class="wallet-name">Dollar</td>
                 <td class="rel-price">Real time price</td>
                 <td class="diagram">Price diagram</td>
             </tr>
-            <tr class="wallet" onclick="location.href = '#'">
+            <tr class="wallet" onclick="address.hidden = false;address.innerHTML = 'wallet address: 3718273918'">
                 <td class="wallet-name">Euro</td>
                 <td class="rel-price">Real time price</td>
                 <td class="diagram">Price diagram</td>
             </tr>
-        </tbody>
-    </table>
-</div>
+            </tbody>
+        </table>
+        <div id="address" hidden>wallet address: </div>
+    </div>
 @stop
 
 
