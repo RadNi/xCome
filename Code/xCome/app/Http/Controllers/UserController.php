d1f39cc845fecf6a34ad67a1bc9bfff7d0b681e0
RadNi <amirhossein_kh@ymail.com>
2018-08-16 15:08:31 +0430

    registration form connected to data base x_users table

--- Code/xCome/app/Http/Controllers/UserController.php
+++ Code/xCome/app/Http/Controllers/UserController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\x_user;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    protected $x_user;
+    function __construct(x_user $x_user)
+    {
+        $this->x_user = $x_user;
+    }
 
     public function showForget() {
         return view("extra.forget", array('check' => false));
@@ -30,11 +37,56 @@ class UserController extends Controller
     }
 
     public function showRegister(Request $request) {
+
         return view("extra.register", array('check' => false));
     }
 
     public function checkRegister(Request $request) {
-        return view("extra.register", array('check' => true));
+
+
+//        <input id="email" type="email" name="email" placeholder="Email"><br>
+//        <input id="password" type="password" name="password" placeholder="Password"><br>
+//        <input id="repass" type="password" name="retryPass" placeholder="Repeat Password"><br>
+//        <input id="name" type="name" name="name" placeholder="Name"><br>
+//        <input id="family" type="name" name="familyName" placeholder="Family"><br>
+//        <input id="username" type="name" name="username" placeholder="Username"><br>
+//        {{--<input id="" type="age" name="age" placeholder="age"><br>--}}
+//        <input id="address" type="address" name="address" placeholder="address"><br>
+//        <input id="captcha" type="text" name="captcha" placeholder="captcha"><br>
+//        <input id="person_id" type="text" name="PersonID" placeholder="Person ID"><br>
+//        <input id="cellphone" type="text" name="CellPhone" placeholder="Phone Number"><br>
+//        <input id="submit" type="submit" value="register">
+
+
+//        dd($request->except(["_token", "captcha", "repass"]));
+//        dd($request -> except(["captcha", "repass"]));
+        $data = $request -> except(["captcha", "repass"]);
+
+        $data['family_name'] = $data['familyName'];
+        unset($data['familyName']);
+        $data['phonenumber'] = $data['CellPhone'];
+        unset($data['CellPhone']);
+        $data['national_id'] = $data['PersonID'];
+        unset($data['PersonID']);
+//        dd($data);
+        $data['password'] = md5($data['password']);
+        $this->x_user->create($data);
+//        $data = $request -> except(["_token", "captcha", "repass"]);
+
+//        return x_user::create([
+//           'email' => $request->email,
+//            'name' => $request->name,
+//            'password' => md5($request->password),
+//            'family' => $request->family,
+//            'username' => $request->username,
+//            'address' => $request->address,
+//            'person_id' => $request->person_id,
+//            'cellphone' => $request->cellphone,
+//        ]);
+
+
+
+//        return view("extra.register", array('check' => true));
     }
 
     public function info(Request $request) {
